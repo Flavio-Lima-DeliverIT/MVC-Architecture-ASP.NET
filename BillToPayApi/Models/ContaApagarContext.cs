bcc83a5ff832077d7ee6f3638e194d3d2686ef21
Flimars <flavio.lima@aluno.riogrande.ifrs.edu.br>
2022-09-01 13:34:32 -0300

    Add project files.

---
+++ BillToPayApi/Models/ContaApagarContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BillToPayApi.Models
+{
+    public class ContaApagarContext : DbContext
+    {
+        public ContaApagarContext(DbContextOptions<ContaApagarContext> options) : base(options)
+        {
+
+        }
+        public DbSet<ContaApagar> ContaApagar { get; set; }
+    }
+}
---
+++ BillToPayApi/Models/ListacontaCadastrada.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BillToPayApi.Models
+{
+    public class ListacontaCadastrada
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal ValorOriginal { get; set; }
+        public decimal ValorCorrigido { get; set; }
+        public int QdeDiasEmAtraso { get; set; }
+        public DateTime DataPagamento { get; set; }
+    }
+}
